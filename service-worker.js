524a065df97f0a5eaf54bfac8a98c1b011200f9a
mosafire-hue <mohamadofdibache@gmail.com>
2026-08-16 10:49:17 +0100

    https://mosafire-hue.github.io/spare-parts-app/

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'spare-parts-v6';
+const urlsToCache = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(n) { return n !== CACHE_NAME; })
+             .map(function(n) { return caches.delete(n); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
